e2983c973e9b279c48cf1c41a5a55efede49bdf4
souravghosh92 <souravghosh90@gmail.com>
2025-08-24 13:26:25 +0530

    Create CustomPlugin.cc

---
+++ CustomPlugin.cc
@@ -0,0 +1,9 @@
+#include "CustomPlugin.h"
+
+QString CustomPlugin::brandName() const {
+    return QStringLiteral("Aeronics GCS");
+}
+
+QString CustomPlugin::organizationName() const {
+    return QStringLiteral("Aeronics Technologies Pvt Ltd");
+}
