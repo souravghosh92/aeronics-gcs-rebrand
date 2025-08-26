2f4406354e7c274c3cf35cc6cfe72d2cf6c2b3ef
souravghosh92 <souravghosh90@gmail.com>
2025-08-26 15:47:56 +0530

    Delete CustomPlugin.cc

--- CustomPlugin.cc
+++
@@ -1,9 +0,0 @@
-#include "CustomPlugin.h"
-
-QString CustomPlugin::brandName() const {
-    return QStringLiteral("Aeronics GCS");
-}
-
-QString CustomPlugin::organizationName() const {
-    return QStringLiteral("Aeronics Technologies Pvt Ltd");
-}
